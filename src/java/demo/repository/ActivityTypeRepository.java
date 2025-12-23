e915e6b00cecffef9d93eb76ab46239dd18f1001
bala-amypo <bala@amypo.in>
2025-12-23 08:06:10 +0000

    User commit

--- src/java/demo/repository/ActivityTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ActivityType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActivityTypeRepository
-        extends JpaRepository<ActivityType, Long> {
-}
