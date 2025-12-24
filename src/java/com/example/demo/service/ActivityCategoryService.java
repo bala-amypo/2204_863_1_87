9a9bd27d83d3a7f8ab57a512540e04454bff7e7d
bala-amypo <bala@amypo.in>
2025-12-24 07:51:35 +0000

    User commit

--- src/java/com/example/demo/service/ActivityCategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.ActivityCategory;
-import java.util.List;
-
-public interface ActivityCategoryService {
-    ActivityCategory createCategory(ActivityCategory category);
-    List<ActivityCategory> getAllCategories();
-}
--- src/java/com/example/demo/service/impl/UserServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.entity.ActivityCategory;
-import java.util.List;
-
-public interface UserServiceImpl {
-    ActivityCategory createCategory(ActivityCategory category);
-    List<ActivityCategory> getAllCategories();
-}
