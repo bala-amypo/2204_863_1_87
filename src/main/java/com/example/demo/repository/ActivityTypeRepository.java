0da7439ca3f7086fec761770f065498e78ea02f7
bala-amypo <bala@amypo.in>
2025-12-25 15:29:50 +0000

    User commit

--- src/main/java/com/example/demo/repository/ActivityTypeRepository.java
+++ src/main/java/com/example/demo/repository/ActivityTypeRepository.java
@@ -3,6 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.entity.ActivityType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ActivityTypeRepository
         extends JpaRepository<ActivityType, Long> {
+
+    List<ActivityType> findByCategory_Id(Long categoryId);
 }
--- src/main/java/com/example/demo/repository/EmissionFactorRepository.java
+++ src/main/java/com/example/demo/repository/EmissionFactorRepository.java
@@ -3,6 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.entity.EmissionFactor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmissionFactorRepository
         extends JpaRepository<EmissionFactor, Long> {
+
+    Optional<EmissionFactor> findByActivityType_Id(Long activityTypeId);
 }
