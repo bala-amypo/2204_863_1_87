6d05a21457d8bbe24299f99848a21cd0f24654a2
bala-amypo <bala@amypo.in>
2025-12-25 15:23:49 +0000

    User commit

--- src/main/java/com/example/demo/service/ActivityTypeService.java
+++ src/main/java/com/example/demo/service/ActivityTypeService.java
@@ -1,12 +1,13 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.ActivityType;
-
 import java.util.List;
 
 public interface ActivityTypeService {
 
-    ActivityType save(ActivityType type);
+    ActivityType createType(Long categoryId, ActivityType type);
 
-    List<ActivityType> getAll();
+    ActivityType getType(Long id);
+
+    List<ActivityType> getTypesByCategory(Long categoryId);
 }
