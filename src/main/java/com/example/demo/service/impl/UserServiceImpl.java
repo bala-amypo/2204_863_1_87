95d52a5ebe5da7b50898d6bee22be3242626808f
bala-amypo <bala@amypo.in>
2025-12-26 03:43:29 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.demo.service.impl;
 
 import com.example.demo.entity.ActivityCategory;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserServiceImpl {
     ActivityCategory createCategory(ActivityCategory category);
     List<ActivityCategory> getAllCategories();
