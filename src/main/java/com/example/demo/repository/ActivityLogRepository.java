155fd081504aa81369f79c50d8cd398085a33669
bala-amypo <bala@amypo.in>
2025-12-26 08:43:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/ActivityLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ActivityLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActivityLogRepository
-        extends JpaRepository<ActivityLog, Long> {
-}
